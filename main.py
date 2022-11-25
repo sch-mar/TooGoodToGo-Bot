155e675f1dadb0c238723f3b01321000fedcd5dd
sch-mar <mar.schneider@gmx.net>
2022-11-25 20:40:31 +0100

    basic logging

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
+import logging
+import logging.config
+import logging.handlers
+import yaml
+
 import multiprocessing
 import time
 
 import tgtglib
 
 if __name__ == '__main__':
+
+    # start logger
+    logging.config.dictConfig(yaml.load(open('log/log.config', 'r'), Loader=yaml.FullLoader))
+    logger = logging.getLogger()
+    logger.info('logger created')
+
     while True:
         p = multiprocessing.Process(target=tgtglib.bot, name="Bot")
         p.start()
