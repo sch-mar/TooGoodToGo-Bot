46416792b804166694627244d0a6ce8ad00f4929
sch-mar <mar.schneider@gmx.net>
2022-12-04 17:22:55 +0100

    Changed config.py for YAML config files

--- src/tgtglib/config.py
+++ src/tgtglib/config.py
@@ -1,22 +1,67 @@
+import yaml
 import configparser
 
 DEFAULT_DIR = 'config'
 
 """
+File specification: YAML
 Hardcoded to '.config' files in different paths.
 No directory/software module should need multiple
 config files.
 """
 
+def get(keys: list, dir=DEFAULT_DIR):
+    path = dir.removesuffix('/') + '/.config'
+    cache = yaml.load(open(path, 'r'), Loader=yaml.FullLoader)
+    for key in keys:
+        cache = cache.get(key)
+    if cache is not None:
+        return cache
+    else:
+        raise KeyError
+
+def set(keys: list, value, dir=DEFAULT_DIR):
+    path = dir.removesuffix('/') + '/.config'
+    cache = yaml.load(open(path, 'r'), Loader=yaml.FullLoader)
+    try:
+        # credit to @Backuriu and @Christopher Shymansky: https://stackoverflow.com/a/56385521
+        sub_d = cache
+        for ind, key in enumerate(keys[:-1]):
+            if not ind:
+                sub_d = cache.setdefault(key, {})
+            else:
+                sub_d = sub_d.setdefault(key, {})
+        sub_d[keys[-1]] = value
+    except:
+        raise KeyError
+    yaml.dump(cache, open(path, 'w'), yaml.Dumper)
+
+def rm(keys: list, dir=DEFAULT_DIR):
+    path = dir.removesuffix('/') + '/.config'
+    cache = yaml.load(open(path, 'r'), Loader=yaml.FullLoader)
+    try:
+        sub_d = cache
+        for ind, key in enumerate(keys[:-1]):
+            if not ind:
+                sub_d = cache.setdefault(key, {})
+            else:
+                sub_d = sub_d.setdefault(key, {})
+        sub_d.pop(keys[-1])
+    except:
+        raise KeyError
+    yaml.dump(cache, open(path, 'w'), yaml.Dumper)
+
+# old functions using ini specification
+
 # get value from config
-def get_config(section, option, dir=DEFAULT_DIR, fallback=None):
+def get_ini(section, option, dir=DEFAULT_DIR, fallback=None):
     path = dir.removesuffix('/') + '/.config'
     config = configparser.ConfigParser()
     config.read(path)
     return config.get(section, option, fallback=fallback)
 
 # change or add option in config
-def set_config(section, option, value, dir=DEFAULT_DIR):
+def set_ini(section, option, value, dir=DEFAULT_DIR):
     path = dir.removesuffix('/') + '/.config'
     config = configparser.ConfigParser()
     config.read(path)
@@ -26,7 +71,7 @@ def set_config(section, option, value, dir=DEFAULT_DIR):
     config.write(open(path, 'w'))
 
 # remove section or option from config
-def rm_config(section, option=None, dir=DEFAULT_DIR):
+def rm_ini(section, option=None, dir=DEFAULT_DIR):
     path = dir.removesuffix('/') + '/.config'
     config = configparser.ConfigParser()
     config.read(path)
